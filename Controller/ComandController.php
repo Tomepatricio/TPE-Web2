57fb661ccb86e4666524b5ebde3176c81cb966fc
Tome Patricio <tomepatricio@gmail.com>
2020-10-14 19:19:37 -0300

    modif access edit y delete from URL

--- Controller/ComandController.php
+++ Controller/ComandController.php
@@ -17,66 +17,92 @@ Class ComandController {
     }
 
  function borrarProducto($params){
+    $userlog=$this->user->checkLog();
+    if ($userlog!=false) {
         $delete=$params[':id'];
         $this->modelProducto->borrarProductoDB($delete);
         $productos=$this->modelProducto->getProductosDB();
-        $this->view->showProductosLocation();
+    }
+    $this->view->showProductosLocation();
     }
 
     function insertarProducto(){
-        $this->modelProducto->agregarProductoDB($_POST['nombreProducto'],$_POST['detalleProducto'],$_POST['presentacionProducto'],$_POST['precioProducto'],$_POST['marcaProducto']);
-        $productos=$this->modelProducto->getProductosDB();
+        $userlog=$this->user->checkLog();
+        if ($userlog!=false) {
+            $this->modelProducto->agregarProductoDB($_POST['nombreProducto'],$_POST['detalleProducto'],$_POST['presentacionProducto'],$_POST['precioProducto'],$_POST['marcaProducto']);
+            $productos=$this->modelProducto->getProductosDB();
+        }
         $this->view->showProductosLocation();
     }
 
     function insertarMarca(){
-        $this->modelMarca->agregarMarcaDB($_POST['nombreMarca'],$_POST['descripcionMarca'],$_POST['origenMarca'],$_POST['puntajeMarca']);
+        $userlog=$this->user->checkLog();
+        if ($userlog!=false) {
+            $this->modelMarca->agregarMarcaDB($_POST['nombreMarca'],$_POST['descripcionMarca'],$_POST['origenMarca'],$_POST['puntajeMarca']);
+        }
         $marcas=$this->modelMarca->getMarcaDB();
         $this->view->showMarcasLocation();
     }
 
     function borrarMarca($params){
-        $delete=$params[':id'];
-        $this->modelMarca->borrarMarcaDB($delete);
+        $userlog=$this->user->checkLog();
+        if ($userlog!=false) {
+            $delete=$params[':id'];
+            $this->modelMarca->borrarMarcaDB($delete);
+        }
         $marcas=$this->modelMarca->getMarcaDB();
         $this->view->showMarcasLocation();
     }
 
     function editarMarca($params){
         $userlog=$this->user->checkLog();
-        $marcas=$this->modelMarca->getMarcaDB();
-        $marca=$params[':id'];
-        foreach ($marcas as $marc) {
-            if($marc->id_marca==$marca){
-                $marca=$marc;
+        if ($userlog!=false) {
+            $marcas=$this->modelMarca->getMarcaDB();
+            $marca=$params[':id'];
+            foreach ($marcas as $marc) {
+                if($marc->id_marca==$marca){
+                    $marca=$marc;
+                }
             }
+            $this->view->renderEditarMarca($marca,$marcas,$userlog);
+        }else{
+            $this->view->renderHome();
         }
-        $this->view->renderEditarMarca($marca,$marcas,$userlog);
     }
 
     function editarYGuardarMarcas(){
-        $this->model->editarMarcaDB($_POST['idMarca'],$_POST['nombreMarca'],$_POST['descripcionMarca'],$_POST['origenMarca'],$_POST['puntajeMarca']);
-        $marcas=$this->modelMarca->getMarcaDB();
+        $userlog=$this->user->checkLog();
+        if ($userlog != false){
+            $this->model->editarMarcaDB($_POST['idMarca'],$_POST['nombreMarca'],$_POST['descripcionMarca'],$_POST['origenMarca'],$_POST['puntajeMarca']);
+            $marcas=$this->modelMarca->getMarcaDB();
+        }
         $this->view->showMarcasLocation();
     }
     
     function editarYGuardarProductos(){//Guarda en Base en datos
-        $this->model->editarProductosDB($_POST['idProducto'],$_POST['nombreProducto'],$_POST['detalleProducto'],$_POST['presentacionProducto'],$_POST['precioProducto'],$_POST['marcaProducto']);
-        $productos=$this->modelProducto->getProductosDB();
+        $userlog=$this->user->checkLog();
+        if ($userlog!=false) {
+            $this->model->editarProductosDB($_POST['idProducto'],$_POST['nombreProducto'],$_POST['detalleProducto'],$_POST['presentacionProducto'],$_POST['precioProducto'],$_POST['marcaProducto']);
+            $productos=$this->modelProducto->getProductosDB();
+        }
         $this->view->showProductosLocation();
     }
 
     function editarProductos($params){
         $userlog=$this->user->checkLog();
-        $productos=$this->modelProducto->getProductosDB();
-        $producto=$params[':id'];
-        foreach ($productos as $prod) {
-            if($prod->id==$producto){
-                $producto=$prod;
+        if ($userlog!=false) {
+            $productos=$this->modelProducto->getProductosDB();
+            $producto=$params[':id'];
+            foreach ($productos as $prod) {
+                if($prod->id==$producto){
+                    $producto=$prod;
+                }
             }
+            $marcas=$this->modelMarca->getMarcaDB();
+            $this->view->renderEditar($producto,$marcas,$userlog);
+        }else{
+            $this->view->renderHome();
         }
-        $marcas=$this->modelMarca->getMarcaDB();
-        $this->view->renderEditar($producto,$marcas,$userlog);
     }
 
 }
--- templates_c/277b6085a7dcc9bd2a8593bfd1755e61eb2e58c1_0.file.descripcionProducto.tpl.php
+++ templates_c/277b6085a7dcc9bd2a8593bfd1755e61eb2e58c1_0.file.descripcionProducto.tpl.php
@@ -1,28 +1,29 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-10-11 23:18:07
+/* Smarty version 3.1.34-dev-7, created on 2020-10-14 22:15:38
   from 'C:\xampp\htdocs\web2\TEST TPE\templates\descripcionProducto.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5f83768f50fee4_56484651',
+  'unifunc' => 'content_5f875c6a6eaf52_84286400',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '277b6085a7dcc9bd2a8593bfd1755e61eb2e58c1' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web2\\TEST TPE\\templates\\descripcionProducto.tpl',
-      1 => 1602451084,
+      1 => 1602464420,
       2 => 'file',
     ),
   ),
   'includes' => 
   array (
     'file:header.tpl' => 1,
+    'file:formEditProducto.tpl' => 1,
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_5f83768f50fee4_56484651 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f875c6a6eaf52_84286400 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -43,53 +44,8 @@ $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_sm
 </div>
 
 <?php if ($_smarty_tpl->tpl_vars['user']->value) {?>
-    <form class="container" action="editar" method="post">
-      <div class="form-group">
-        <small class="form-text text-muted">ID</small>
-        <input type="hidden" class="form-control" name="idProducto" value=<?php echo $_smarty_tpl->tpl_vars['producto']->value->id;?>
->
-      </div>
-      <div class="form-group">
-        <small class="form-text text-muted">Nombre del Producto</small>
-        <input type="text" class="form-control" name="nombreProducto" value=<?php echo $_smarty_tpl->tpl_vars['producto']->value->nombre;?>
->
-      </div>
-      <div class="form-group">
-        <small class="form-text text-muted">Descripcion del Producto</small>
-        <input type="text" class="form-control" name="detalleProducto" value=<?php echo $_smarty_tpl->tpl_vars['producto']->value->detalle;?>
->
-      </div>
-      <div class="form-group">
-        <small class="form-text text-muted">Presentacion del Producto</small>
-        <input type="number" class="form-control" name="presentacionProducto" value=<?php echo $_smarty_tpl->tpl_vars['producto']->value->presentacion;?>
->
-      </div>
-      <div class="form-group">
-        <small class="form-text text-muted">Precio del Producto</small>
-        <input type="number" class="form-control" name="precioProducto" value=<?php echo $_smarty_tpl->tpl_vars['producto']->value->precio;?>
->
-      </div>
-      <div class="form-group">
-      <small class="form-text text-white">Marca del Producto</small>
-      <select name="marcaProducto">
-          <?php
-$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['marcas']->value, 'marca', false, 'key', 'name', array (
-));
-$_smarty_tpl->tpl_vars['marca']->do_else = true;
-if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['key']->value => $_smarty_tpl->tpl_vars['marca']->value) {
-$_smarty_tpl->tpl_vars['marca']->do_else = false;
-?>
-              <option name="marcaProducto" value=<?php echo $_smarty_tpl->tpl_vars['marca']->value->id_marca;?>
-><?php echo $_smarty_tpl->tpl_vars['marca']->value->nombreMarca;?>
-</option>
-          <?php
-}
-$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
-      </select>
-      </div>
-      <button type="submit" class="btn btn-warning">Editar</button>
-    </form>
-<?php }?>
+    <?php $_smarty_tpl->_subTemplateRender("file:formEditProducto.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}?>
 
 <?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 }
