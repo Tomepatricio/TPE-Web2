2f63e134ff854dbb4f840f8197ed5a2822e115d4
ptome@summasolutions.net <ptome@summasolutions.net>
2021-05-24 18:46:32 -0300

    Se resuelven conflictos en RouteApi.php

--- RouteApi.php
+++ RouteApi.php
@@ -3,22 +3,17 @@
 require_once 'RouterClass.php';
 require_once './Controller/ApiController.php';
 
-<<<<<<< HEAD
-=======
 define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
 
->>>>>>> develop
 $r = new Router();
 
 $r->addRoute("productos", "GET", "ApiController", "getProductos");
 $r->addRoute("productos/:id", "GET", "ApiController", "getProductosById");
 $r->addRoute("productos/:id", "DELETE", "ApiController", "deleteProducto");
-<<<<<<< HEAD
 
 $r->addRoute("productos", "POST", "ApiController", "insertarProducto");
 $r->addRoute("productos/:id", "PUT", "ApiController", "updateProducto");
 
-=======
 $r->addRoute("productos", "POST", "ApiController", "insertarProducto");
 $r->addRoute("productos/:id", "PUT", "ApiController", "updateProducto");
 
@@ -26,6 +21,4 @@ $r->addRoute("comment","POST","ApiController", "sendComment");
 $r->addRoute("comment","GET","ApiController", "getComment");
 $r->addRoute("comment/:id", "DELETE", "ApiController", "deleteComment");
 
->>>>>>> develop
-//run
-$r->route($_GET['resource'], $_SERVER['REQUEST_METHOD']); 
+$r->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
